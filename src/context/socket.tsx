8181e94634a29db52363ecc044cb17dca437c332
Ewan May <ewan.may@ucalgary.ca>
2021-04-14 00:23:02 -0600

    update socket transport method

--- src/context/socket.tsx
+++ src/context/socket.tsx
@@ -1,3 +1,3 @@
 import Socket from 'socket.io-client'
-export const socket: SocketIOClient.Socket = Socket('http://localhost:5000');
-
+const route = process.env.REACT_APP_ROUTE ? process.env.REACT_APP_ROUTE : 'http://localhost:5000'
+export const socket: SocketIOClient.Socket = Socket(route, { transports: ["websocket", "polling", "flashsocket"]});
